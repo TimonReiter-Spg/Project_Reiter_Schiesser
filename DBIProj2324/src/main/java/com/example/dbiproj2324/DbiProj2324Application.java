2056ae1dbcb57f34276b03f4ca607e2d617223cd
Johannes <sch19696@spengergasse.at>
2023-12-15 22:38:37 +0100

    sicherheitshalber

---
+++ DBIProj2324/src/main/java/com/example/dbiproj2324/DbiProj2324Application.java
@@ -0,0 +1,13 @@
+package com.example.dbiproj2324;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbiProj2324Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbiProj2324Application.class, args);
+    }
+
+}
---
+++ DBIProj2324/src/test/java/com/example/dbiproj2324/DbiProj2324ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbiproj2324;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbiProj2324ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
